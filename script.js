d51aeb109f59bacb69568c84f644f48fe2848344
leandsonpy <leandsoncarvalho07@gmail.com>
2025-11-12 16:13:39 -0300

    Alteração HTML e CSS

---
+++ script.js
@@ -0,0 +1,13 @@
+const apiUrl = 'httpp://localhost:3000/tasks';
+
+const form = document.getElementById('task-form');
+const taskList = document.getElementById ('task-list');
+
+form.addEventListener('submit',async (e) =>{
+e.preventDefault();
+
+
+
+const title = document.getElementById('title').value;
+const description = document.getElementById('description').value;
+})
