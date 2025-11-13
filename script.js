87945679546aa5c11a8a7b9fbcca717f9263ad88
leandsonpy <leandsoncarvalho07@gmail.com>
2025-11-13 14:57:55 -0300

    criação funções script.js

--- script.js
+++ script.js
@@ -6,8 +6,21 @@ const taskList = document.getElementById ('task-list');
 form.addEventListener('submit',async (e) =>{
 e.preventDefault();
 
-
+form.addEventListener('submit', async (e)=>{
+e.preventDefault();
+})
 
 const title = document.getElementById('title').value;
 const description = document.getElementById('description').value;
+
+try{
+    const res = await fetch(apiUrl,{
+    method: 'POST',
+    headers: {'content-type': 'application/json'},
+    body: JSON.stringify({title, description})
+    })
+
+}catch (error){
+
+}
 })
