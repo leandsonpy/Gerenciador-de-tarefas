1ae9e7c55814eb6cbd49357431e156141ebf7803
leandsonpy <leandsoncarvalho07@gmail.com>
2025-11-13 15:55:44 -0300

    script finalizado

--- script.js
+++ script.js
@@ -1,26 +1,87 @@
 const apiUrl = 'httpp://localhost:3000/tasks';
 
 const form = document.getElementById('task-form');
-const taskList = document.getElementById ('task-list');
+const taskList = document.getElementById('task-list');
 
-form.addEventListener('submit',async (e) =>{
-e.preventDefault();
+form.addEventListener('submit', async (e) => {
+    e.preventDefault();
 
-form.addEventListener('submit', async (e)=>{
-e.preventDefault();
-})
-
-const title = document.getElementById('title').value;
-const description = document.getElementById('description').value;
-
-try{
-    const res = await fetch(apiUrl,{
-    method: 'POST',
-    headers: {'content-type': 'application/json'},
-    body: JSON.stringify({title, description})
+    form.addEventListener('submit', async (e) => {
+        e.preventDefault();
     })
 
-}catch (error){
+    const title = document.getElementById('title').value;
+    const description = document.getElementById('description').value;
 
+    try {
+        const res = await fetch(apiUrl, {
+            method: 'POST',
+            headers: { 'content-type': 'application/json' },
+            body: JSON.stringify({ title, description })
+        })
+
+        if (!res.ok) throw new Error("Erro ao adicionar tarefa");
+
+
+        const task = await res.json();
+        form.reset();
+        addTaskToUl(task);
+
+
+    } catch (error) {
+        alert("Erro ao salvar tarefa: " + error.message);
+    }
+});
+
+function addTaskToUl(task) {
+    const li = document.createElement("li");
+    li.classname = task.completed ? "completed" : "";
+    li.innerHTML = `
+<span>${task.title} - ${task.description}</span>
+<div>
+ <button onClick= "toggleCompleted(${task.id},${task.completed})">☑️</button>
+ <button onClick= "deleteTask(${task.id}")>🗑️</button>
+</div>
+`;
+taskList.appendChild(li);
 }
-})
+
+
+async function loadTask() {
+ try{
+   const res = await fetch(apiUrl);
+   if(!res.ok) throw new Error("Erro ao carregar tarefas");
+   const tasks = await res.json();
+   taskList.innerHTML = "";
+   task.forEach(addTaskToUl);
+    
+   } catch (erro) {
+
+alert ("Erro ao carregar tarefas: " + error.message);
+ }  
+}
+
+async function toggleCompleted(id,completed){
+  try{
+await fetch(`${apiUrl}/${id}`,{
+   method: "PUT",
+   headers: {"Content-Type": "application/json"},
+   body: JSON.stringify({completed: !completed}) 
+});
+loadTask();
+
+  }catch (error) {
+alert("Erro ao atualizar tarefa: " + error.message);
+  }
+}
+
+async function deleteTask(id) {
+  try{
+    await fetch(`${apiUrl}/${id}`, {method:`DELETE`});
+    loadTask();
+    }catch (error){
+      alert("Erro ao excluir tarefa:" + erro.message);
+    
+  }
+}
+localTask();
